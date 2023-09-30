d14db2a733657fd51e923370266ff90d22e1b8e8
Ben Wu <wubin28@gmail.com>
2023-09-30 21:51:19 +0800

    feat: We can listen to DOM events using the v-on directive. Due to its frequent use, v-on also has a shorthand syntax. Event handlers can also use inline expressions, and can simplify common tasks with modifiers.

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,17 @@
 import { ref } from 'vue'
 
 const count = ref(0)
+
+function increment() {
+  // update component state
+  count.value++
+}
 </script>
 
 <template>
   <!-- make this button work -->
-  <button>count is: {{ count }}</button>
+  <button v-on:click="increment">count is: {{ count }}</button>
+  <!-- Or <button @click="increment">count is: {{ count }}</button> -->
 </template>
 
 <style scoped></style>
