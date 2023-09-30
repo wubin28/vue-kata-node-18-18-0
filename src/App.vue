49c031bc244804eb07c278ad79646ed1b65ab9d3
Ben Wu <wubin28@gmail.com>
2023-09-30 22:36:00 +0800

    chore: prepare for step 7

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,42 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
+// give each todo a unique id
+let id = 0
+
+const newTodo = ref('')
+const todos = ref([
+  { id: id++, text: 'Learn HTML' },
+  { id: id++, text: 'Learn JavaScript' },
+  { id: id++, text: 'Learn Vue' }
+])
+
+function addTodo() {
+  // ...
+  newTodo.value = ''
+}
+
+interface Todo {
+  id: number
+  text: string
+}
+
+function removeTodo(todo: Todo) {
+  // ...
+}
 </script>
 
 <template>
-  <h1>Hello World!</h1>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo" />
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
