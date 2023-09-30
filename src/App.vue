628332bbd342c9b8d7a1a125f6e48d9921994633
Ben Wu <wubin28@gmail.com>
2023-09-30 20:33:21 +0800

    feat: State that can trigger updates when changed are considered reactive. We can declare reactive state using Vue's reactive() API. Objects created from reactive() are JavaScript Proxies that work just like normal objects. reactive() only works on objects (including arrays and built-in types like Map and Set). ref(), on the other hand, can take any value type and create an object that exposes the inner value under a .value property. Reactive state declared in the component's <script setup> block can be used directly in the template. This is how we can render dynamic text based on the value of the counter object and message ref, using mustaches syntax. Notice how we did not need to use .value when accessing the message ref in templates: it is automatically unwrapped for more succinct usage.The content inside the mustaches is not limited to just identifiers or paths - we can use any valid JavaScript expression.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import {reactive, ref} from 'vue'
 
 // component logic
 // declare some reactive state here.
+const counter = reactive({count: 0})
+
+console.log(counter.count) // 0
+counter.count++ // 1
+
+const message = ref('Hello World!')
+
+console.log(message.value) // Hello World!
+message.value = 'Changed' // Changed
 </script>
 
 <template>
-  <h1>Make me dynamic!</h1>
+  <h1>{{ message }}</h1>
+  <h1>{{ message.split('').reverse().join('') }}</h1>
+  <p>count is: {{ counter.count}}</p>
 </template>
 
 <style scoped></style>
