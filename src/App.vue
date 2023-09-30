919177da3a1fa81d742869a15eab7ddd3e9446be
Ben Wu <wubin28@gmail.com>
2023-09-30 22:20:42 +0800

    chore: prepare for step 6

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
+const awesome = ref(true)
+
+function toggle() {
+  // ...
+}
 </script>
 
 <template>
-  <h1>Hello World!</h1>
+  <button @click="toggle">toggle</button>
+  <h1>Vue is awesome!</h1>
+  <h1>Oh no 😢</h1>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
