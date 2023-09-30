fef3a1d8a090312d1f01d4ba075ef7f66d3215bc
Ben Wu <wubin28@gmail.com>
2023-09-30 21:39:30 +0800

    feat: In Vue, mustaches are only used for text interpolation. To bind an attribute to a dynamic value, we use the v-bind directive. A directive is a special attribute that starts with the v- prefix. They are part of Vue's template syntax. Similar to text interpolations, directive values are JavaScript expressions that have access to the component's state. The part after the colon (:id) is the 'argument' of the directive. Here, the element's id attribute will be synced with the dynamicId property from the component's state.Because v-bind is used so frequently, it has a dedicated shorthand syntax.

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@ const titleClass = ref('title')
 </script>
 
 <template>
-  <h1>Make me red</h1>
+  <h1 v-bind:class="titleClass">Make me red</h1>
+  <!-- or <h1 :class="titleClass">Make me red</h1> -->
   <!-- add dynamic class binding here -->
 </template>
 
