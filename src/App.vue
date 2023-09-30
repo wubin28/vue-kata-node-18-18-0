8af77fd2210d369c119d0adc1509c5c258e21d36
Ben Wu <wubin28@gmail.com>
2023-09-30 21:30:16 +0800

    chore: prepare for step 3

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
+const titleClass = ref('title')
 </script>
 
 <template>
-  <h1>Hello World!</h1>
+  <h1>Make me red</h1>
+  <!-- add dynamic class binding here -->
 </template>
 
 <style scoped>
+.title {
+  color: red;
+}
 </style>
