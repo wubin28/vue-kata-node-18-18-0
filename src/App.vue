47089ed04ee2512ac37629fbf1736556fbaba796
Ben Wu <wubin28@gmail.com>
2023-09-30 22:02:50 +0800

    chore: prepare for step 5

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
+const text = ref('')
+
+function onInput(e: Event) {
+  text.value = (e.target as HTMLInputElement).value
+}
 </script>
 
 <template>
-  <h1>Hello World!</h1>
+  <input :value="text" @input="onInput" placeholder="Type here" />
+  <p>{{ text }}</p>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
