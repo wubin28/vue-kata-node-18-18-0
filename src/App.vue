d28e3f9823fb1ee22e982d4f2a48e029454072a7
Ben Wu <wubin28@gmail.com>
2023-10-01 09:32:26 +0800

    feat: Introducing computed(). We can create a computed ref that computes its .value based on other reactive data sources. A computed property tracks other reactive state used in its computation as dependencies. It caches the result and automatically updates it when its dependencies change.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 let id = 0
 
@@ -10,6 +10,9 @@ const todos = ref([
   { id: id++, text: 'Learn JavaScript', done: true },
   { id: id++, text: 'Learn Vue', done: false }
 ])
+const filteredTodos = computed(() => {
+  return hideCompleted.value ? todos.value.filter((t) => !t.done) : todos.value
+})
 
 function addTodo() {
   todos.value.push({ id: id++, text: newTodo.value, done: false })
@@ -33,7 +36,7 @@ function removeTodo(todo: Todo) {
     <button>Add Todo</button>
   </form>
   <ul>
-    <li v-for="todo in todos" :key="todo.id">
+    <li v-for="todo in filteredTodos" :key="todo.id">
       <input type="checkbox" v-model="todo.done" />
       <span :class="{ done: todo.done }">{{ todo.text }}</span>
       <button @click="removeTodo(todo)">X</button>
