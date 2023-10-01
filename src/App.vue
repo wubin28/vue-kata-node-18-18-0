ca7e3a4c0c55e0136325088dea7766be6bd2e1ed
Ben Wu <wubin28@gmail.com>
2023-10-01 09:12:25 +0800

    chore: prepare for step 8

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,51 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
+let id = 0
+
+const newTodo = ref('')
+const hideCompleted = ref(false)
+const todos = ref([
+  { id: id++, text: 'Learn HTML', done: true },
+  { id: id++, text: 'Learn JavaScript', done: true },
+  { id: id++, text: 'Learn Vue', done: false }
+])
+
+function addTodo() {
+  todos.value.push({ id: id++, text: newTodo.value, done: false })
+  newTodo.value = ''
+}
+
+interface Todo {
+  id: number
+  text: string
+  done: boolean
+}
+
+function removeTodo(todo: Todo) {
+  todos.value = todos.value.filter((t) => t !== todo)
+}
 </script>
 
 <template>
-  <h1>Hello World!</h1>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo" />
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      <input type="checkbox" v-model="todo.done" />
+      <span :class="{ done: todo.done }">{{ todo.text }}</span>
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
+  <button @click="hideCompleted = !hideCompleted">
+    {{ hideCompleted ? 'Show all' : 'Hide completed' }}
+  </button>
 </template>
 
 <style scoped>
+.done {
+  text-decoration: line-through;
+}
 </style>
