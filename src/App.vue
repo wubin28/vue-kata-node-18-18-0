916a3e0cf4f5f0603e18a6a0cb46b5297e4a97c6
Ben Wu <wubin28@gmail.com>
2023-10-01 13:55:51 +0800

    feat: So far, we've only been working with a single component. Real Vue applications are typically created with nested components. A parent component can render another component in its template as a child component. To use a child component, we need to first import it. Then, we can use the component in the template as: <ChildComp />.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ChildComponent from '@/ChildComponent.vue'
+</script>
 
 <template>
   <!-- render child component -->
+  <ChildComponent />
 </template>
 
 <style scoped></style>
